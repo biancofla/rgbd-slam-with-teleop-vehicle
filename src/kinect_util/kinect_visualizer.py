cec7c4453de0ba07e79536daee855a5a848175c8
Flavio Biancospino <flavio.biancospino@gmail.com>
2020-06-19 18:48:15 +0200

    Add comment to kinect_visualizer.py

--- src/kinect_util/kinect_visualizer.py
+++ src/kinect_util/kinect_visualizer.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
-from std_msgs.msg import String
 import roslib
 import rospy
 import cv2
-import sys
 
 class kinect_visualizer:
 
-  def __init__(self):
-    rospy.init_node('kinect_visualizer', anonymous=True)
-    self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("/rgbd_message", Image, self._callback)
+	def __init__(self):
+		# Define a new ROS node.
+		rospy.init_node('kinect_visualizer', anonymous=True)
+		# Create the CvBridge object.
+    	self.bridge = CvBridge()
+		# Subscribe to the /rgbd_message topic.
+    	self.image_sub = rospy.Subscriber("/rgbd_message", Image, self._callback)
 
-  def _callback(self, data):
-    try:
-      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-      
-      cv2.imshow("rgbd", cv_image)
-      cv2.waitKey(30)
-      
-      rospy.loginfo('I just retrive a RGBD image.')
-    except CvBridgeError as e:
-        rospy.logerr(e)
+	def _callback(self, data):
+		rospy.loginfo('I just receive a RGBD image.')
 
-def main(args):
-  try:
-    kinect_visualizer()
-    rospy.spin()
-  except KeyboardInterrupt:
-    cv2.destroyAllWindows()
+    	try:
+			# Use cv_bridge() to convert the ROS image to OpenCV format.
+      		cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+			# Visualize received image.
+      		cv2.imshow("RGBD", cv_image)
+      		cv2.waitKey(30)
+    	except CvBridgeError as e:
+        	rospy.logerr(e)
+
+def main():
+  	try:
+    	kinect_visualizer()
+    	rospy.spin()
+	except KeyboardInterrupt:
+    	cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    main(sys.argv)
+	main()
